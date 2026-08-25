0cdb0995240d1aac96b3b3afee8fd37fb4168a39
Luxxool <luc.soulerin@oclock.school>
2026-08-25 17:06:58 +0000

    Adjustement and relooking

--- bash/README.md
+++ bash/README.md
@@ -1,16 +1,18 @@
 
 # SB4-Scripts-luxxool
 
-Le Script installe 4 éditeurs en ligne de commande afin de pouvoir faire sur une même machine des tests et des commparaisons :
+## install-editors-toolset.sh
 
-## les Editeurs
+### Ce script installe 4 éditeurs en ligne de commande afin de pouvoir faire sur une même machine des tests et des commparaisons :
+
+### les Editeurs
 * The Nice Editor , taper ``ne  <file name>``
 * GNU Emacs , taper: ``emacs  <file name>``
 * Micro , taper ``mc  <file name>``
-* Helix , taper ``helix  <file name>``
+* Helix , taper ``helix  <file name>`` Not yet implemented
 
 et 
-## Midnight Commander
+###  Midnight Commander
  * mcedit, taper :``mcedit  <file name>`` pour editer
  *  ou juste ``mc`` et naviguer dans l'arboressence des fichiers avec une interface plus graphique !
 
--- bash/install-editors-toolset.sh
+++ bash/install-editors-toolset.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ "$(id -u)" -eq 0 ]; then
+  sudo() { "$@"; }
+fi
+
 
 echo "Installing Nice Editor (Ne) "
 sudo apt install ne
@@ -8,16 +15,22 @@ sudo apt install emacs
 echo "Installing Micros"
 sudo apt install micro
 
-echo "Installing Helix "
-cd  ~é
-sudo apt install git cargo
-git clone https://github.com/helix-editor/helix
-cd helix
-cargo install --path helix-term
-ln -s $PWD/runtime ~/.config/helix/runtime
-export PATH=$PATH:/home/team/.cargo/bin
+# ekho "Installing Helix "
+# cd ~
+# sudo apt install git cargo
+# git clone https://github.com/helix-editor/helix
+# cd helix
+# cargo install --path helix-term
+# ln -s $PWD/runtime ~/.config/helix/runtime
+# export PATH=$PATH:/home/team/.cargo/bin
 
 
 echo "Installing Micros"
 sudo apt install micro
 
+echo "##HH||             #|               #|      #HH|              #|         #|  #|           H| "
+echo "  #|    #H|   #H|  #|   #HH|  #H|  ##HH|     #|  ##H|   #HH| ##HH| ##|   #|  #|   #H|     H| "
+echo "  #|   ## H| ## H| #|  ##H|  ##HH|  #|       #|  ## H| ##H|   #|     H|  #|  #|  ##HH|  #HH| "
+echo "  #|   ## H| ## H| #|     H| ##     #|       #|  ## H|    H|  #|   ##H|  #|  #|  ##    ## H| " 
+echo "  #|    #H|   #H|  #H| ##H|   #HH|  #H|     #HH| ## H| ##H|   #H|  ##HH| #H| #H|  #HH|  #HH| "
+                                                                                             
